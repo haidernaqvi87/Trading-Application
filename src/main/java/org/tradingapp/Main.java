dd10017bc80b54894382b8e834a8ff336f6606e9
Haider <hn@reachtag.de>
2023-07-18 01:34:28 +0200

    its time to write the main method. I created dependencies, of course we can use some service locator packages. Then i set some signals in our application

--- src/main/java/org/tradingapp/Main.java
+++ src/main/java/org/tradingapp/Main.java
@@ -1,5 +1,24 @@
 package org.tradingapp;
 
+import org.tradingapp.external.Algo;
+import org.tradingapp.services.RestDataService;
+import org.tradingapp.signals.Signal0;
+import org.tradingapp.signals.Signal1;
+import org.tradingapp.signals.Signal2;
+
 public class Main {
+    public static void main(String[] args) {
+        Application app = new Application();
+        RestDataService api = new RestDataService();
+        Algo algo = new Algo();
+
+        // THE APP HAS TOTAL 10 Signals
+        // Here I can set channels from 0...9
+        // if all 10 signals are not set then the remaining single position will be of NO SIGNAL
+        app.setSignal(0, new Signal0(algo));
+        app.setSignal(1, new Signal1(algo));
+        app.setSignal(2, new Signal2(algo));
+
+    }
 
 }
